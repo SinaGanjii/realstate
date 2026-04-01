264da9663707e89967b36d61b0fecfd32fce3f45
Sina Ganji <sina7ganji7@gmail.com>
2026-04-01 14:04:37 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig = {
   output: "export",
-  basePath: "/realstate",
-  assetPrefix: "/realstate/",
+  basePath: isProd ? "/realstate" : "",
+  assetPrefix: isProd ? "/realstate/" : "",
   typescript: {
     ignoreBuildErrors: true,
   },
